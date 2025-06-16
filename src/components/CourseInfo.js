7c8c4dd4055300fa693e3535bde2ca561325a1c1
SreekarSBS <sbssreekar@gmail.com>
2025-06-16 23:58:29 +0530

    Students already Bought

--- src/components/CourseInfo.js
+++ src/components/CourseInfo.js
@@ -9,9 +9,12 @@ import Personal from "./Personal";
 import Teams from "./Teams";
 import usecourseInfo3 from "../utils/useCourseInfo3";
 import AccordionContent from "./AccordionContent";
+import useCourseInfo4 from "../utils/useCourseInfo4";
+
 
 const CourseInfo = () => {
     
+  
     const [showAccordion , setShowAccordion] = useState(null);
     const [index , setIndex] = useState();
     const [isPersonal , setIsPersonal] = useState(true);
@@ -20,14 +23,16 @@ const CourseInfo = () => {
     const courseData = useCourseInfo(id);
     const courseData2 = useCourseInfo2(id);
     const courseData3 = usecourseInfo3(id);
+    const already = useCourseInfo4(id);
     const instructors = courseData.visible_instructors?.map((item) => item.display_name );
+
+    
 
     const handleOverflow = () => {
      setOverflow(true)
     }
     const handleClick = (idx) => {
      setShowAccordion((prev) => prev == idx ? null : idx) 
-     
     }
      const  secondsToHms = (d) => {
       d = Number(d);
@@ -92,11 +97,44 @@ const CourseInfo = () => {
                       </div>
                      
                   }) }
+                   {overflow && <div className="mt-4  h-96 bg-red-500">
+              {already.map((item) => <div key = {item.id}>{item.title}</div>)}
+            </div>}
             </div>
             {!overflow && <div onClick={handleOverflow} className=" mt-4 w-[940px] text-center rounded-lg text-amber-600  border border-amber-600 p-4 my-1.5 text-xl font-semibold cursor-pointer hover:bg-amber-50">
             {  courseData3?.curriculum_context?.data?.sections.length > 12 && courseData3?.curriculum_context?.data?.sections.length - 12  } More Sections
             </div>
             }
+             {!overflow && <div className="mt-18   ">
+              <p className=" mb-6 text-4xl font-bold  text-gray-500">Students also Bought</p>
+              {already?.map((item) => <div className="flex justify-evenly border-b-2 w-[1000px] border-gray-300  m-4 p-4 text-xl" key = {item.id}>
+                
+                <img className="w-50" src = {item.image_304x171}  />
+               <div className="m-2 p-2 font-lg font-bold"> {item.title}
+               
+               
+              <div className={`w-36 mt-2 max-h-14 overflow-hidden pt-2 text-center ml-4 rounded-2xl h-10 ${item?.bestseller_badge_content?.badge_text === "Bestseller" ? " bg-green-400" : "bg-amber-400"}  text-black  border font-stretch-110%  border-amber-300`}>{  item?.bestseller_badge_content?.badge_text || "Premium" }   </div>   
+               
+               </div>
+               <div className="m-2 p-2 text-amber-600 font-semibold">{item.avg_rating.toFixed(1)} 🌟</div>
+               <div className="m-2 p-2 font-extralight">{item?.num_subscribers?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+               <div className="cursor-pointer   my-4 py-2  font-bold  text-center rounded-2xl border border-amber-600 hover:bg-amber-400">
+ <svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 128 128"
+  width="50"
+  height="24"
+>
+  <path
+    d="M38.4 63.3s9.2-6.6 15.3-15.7c5.9-8.7 8.8-15.2 8.8-15.2s2.5-8.5.6-17.7c-1-5 4.3-8 10.7-5.9 5.1 1.6 7.5 13 7.9 15.9 1.9 14.2-4.5 28.9-4.5 28.9H115s13.4-.3 13 8.2c-.5 9.8-16.3 9.4-16.3 9.4s10.5 1.4 10.5 8.8c-.1 7.4-12.8 9.2-12.8 9.2s9.2 1.7 8.6 7.6c-.6 5.5-11.4 7.4-11.4 7.4s6.7 1.8 5.7 8.1c-.8 4.9-8.2 4.6-8.2 4.6l-65.5-.1V63.3zm-2.6-2.7v59.3H5.2L0 60.6h35.8zm-8.9 46.9c0-3-2.4-5.4-5.4-5.4-3 0-5.4 2.4-5.4 5.4 0 3 2.4 5.4 5.4 5.4 3 .1 5.4-2.4 5.4-5.4z"
+    fill="#231f20"
+  />
+</svg>
+
+ </div>
+                </div>)
+                }
+            </div>}
          </div>
 
 
@@ -114,7 +152,7 @@ const CourseInfo = () => {
             </div>
            
             {isPersonal ? <Personal courseData = {courseData}  courseData2 = {courseData2} courseData3 = {courseData3} /> : <Teams/>}
-
+            
          </div>
    </div>
 }
---
+++ src/utils/useCourseInfo4.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+
+const useCourseInfo4 = (id) => {
+     const [already,setAlready] = useState();
+
+     useEffect( () => {
+       fetchData();
+     },[])
+
+     const fetchData = async() => {
+        const data = await fetch("https://www.udemy.com/api-2.0/discovery-units/?context=landing-page&from=0&page_size=6&item_count=18&course_id=" +id + "&source_page=course_landing_page&locale=en_US&currency=inr&navigation_locale=en_US&skip_price=true");
+        const jsonData = await data.json();
+        setAlready(jsonData?.units?.[1]?.items)
+        console.log(already);
+        
+     }
+
+    return already
+}
+
+export default useCourseInfo4
